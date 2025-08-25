c635bdd75ad4ff60576380bfd9e5dad5f988a9e9
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-08-25 09:48:26 +0200

    added generated-icons.ts skeleton

---
+++ resources/js/lib/generated-icons.ts
@@ -0,0 +1,11 @@
+import { type LucideIcon } from 'lucide-react';
+
+export const iconMap: Record<string, LucideIcon> = {
+    //
+};
+
+export const getIcon = (name: string): LucideIcon => {
+    return iconMap[name] || iconMap[Object.keys(iconMap)[0]];
+};
+
+export type IconName = keyof typeof iconMap;
