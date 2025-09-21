df56ed95b8bee45f4e5c0cf6c48f555206dac8dd
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-09-21 09:39:34 -0400

    stop tracking generated icons file

--- resources/js/lib/generated-icons.ts
+++
@@ -1,9 +0,0 @@
-import { type LucideIcon } from 'lucide-react';
-
-export const iconMap: Record<string, LucideIcon> = {};
-
-export const getIcon = (name: string): LucideIcon => {
-    return iconMap[name] || iconMap[Object.keys(iconMap)[0]];
-};
-
-export type IconName = keyof typeof iconMap;
