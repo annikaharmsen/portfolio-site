8995ab5f93873141e0c3204ff01e8c5714dabb84
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-09-23 10:47:00 -0400

    modified tag edit pages' delete button and handler

--- resources/js/pages/admin/skills/edit.tsx
+++ resources/js/pages/admin/skills/edit.tsx
@@ -2,9 +2,10 @@ import { DeleteButton } from '@/components/app-buttons';
 import { H1 } from '@/components/headings';
 import TagForm from '@/components/tags/form';
 import { useBreadcrumbs } from '@/hooks/use-breadcrumbs';
+import useController from '@/hooks/use-controller';
 import AppLayout from '@/layouts/app-layout';
 import { Projects, Skill } from '@/types/models';
-import { Head, router } from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
 
 interface EditSkillProps {
     skill: Skill;
@@ -14,18 +15,17 @@ interface EditSkillProps {
 export default function EditSkill({ skill, projects }: EditSkillProps) {
     const breadcrumbs = useBreadcrumbs().getBreadcrumbs('edit_skill', skill);
 
-    const handleDelete = () => {
-        if (skill && confirm('Are you sure you want to delete this skill?')) {
-            router.delete(`/skills/${skill.id}`);
-            router.get('/skills');
-        }
-    };
+    const controller = useController('skills');
+    const handleDelete = () => controller.delete(skill);
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Edit Skill" />
             <div className="m-12 *:mb-12 md:min-w-160">
-                <H1>Edit Skill</H1>
+                <div className="flex items-center justify-between *:my-0">
+                    <H1>Edit Skill</H1>
+                    <DeleteButton onClick={handleDelete} />
+                </div>
                 <TagForm tag={skill} baseURI="skills" projects={projects} />
                 <DeleteButton onClick={handleDelete} />
             </div>
--- resources/js/pages/admin/technologies/edit.tsx
+++ resources/js/pages/admin/technologies/edit.tsx
@@ -2,9 +2,10 @@ import { DeleteButton } from '@/components/app-buttons';
 import { H1 } from '@/components/headings';
 import TagForm from '@/components/tags/form';
 import { useBreadcrumbs } from '@/hooks/use-breadcrumbs';
+import useController from '@/hooks/use-controller';
 import AppLayout from '@/layouts/app-layout';
 import { Projects, Technology } from '@/types/models';
-import { Head, router } from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
 
 interface EditTechnologyProps {
     technology: Technology;
@@ -14,20 +15,18 @@ interface EditTechnologyProps {
 export default function EditTechnology({ technology, projects }: EditTechnologyProps) {
     const breadcrumbs = useBreadcrumbs().getBreadcrumbs('edit_technology', technology);
 
-    const handleDelete = () => {
-        if (technology && confirm('Are you sure you want to delete this technology?')) {
-            router.delete(`/technologies/${technology.id}`);
-            router.get('/technologies');
-        }
-    };
+    const controller = useController('technologies');
+    const handleDelete = () => controller.delete(technology);
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Edit Technology" />
             <div className="m-12 *:mb-12 md:min-w-160">
-                <H1>Edit Technology</H1>
+                <div className="flex items-center justify-between *:my-0">
+                    <H1>Edit Technology</H1>
+                    <DeleteButton onClick={handleDelete} />
+                </div>
                 <TagForm tag={technology} baseURI="technologies" projects={projects} />
-                <DeleteButton onClick={handleDelete} />
             </div>
         </AppLayout>
     );
