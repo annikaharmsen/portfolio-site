1eed7707bc5b17927b4f16b5b753ca665d53f2d0
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-09-23 11:13:10 -0400

    updated project card skill list styling

--- resources/js/components/portfolio/project-card.tsx
+++ resources/js/components/portfolio/project-card.tsx
@@ -57,7 +57,7 @@ export default function ProjectCard({ project }: { project: Project }) {
 
                 {/* skill list */}
                 {!!project.skills?.length && (
-                    <div className="grid grid-cols-2 gap-4">
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                         <IconList items={project.skills} />
                     </div>
                 )}
