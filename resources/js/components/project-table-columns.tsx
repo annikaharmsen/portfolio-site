a4fabfd67ac8fdefbc5589853886ed88443814df
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-09-23 09:43:16 -0400

    updated project featured star button

--- resources/js/components/project-table-columns.tsx
+++ resources/js/components/project-table-columns.tsx
@@ -2,16 +2,17 @@ import { cn } from '@/lib/utils';
 import { Project } from '@/types/models';
 import { router } from '@inertiajs/react';
 import { Star } from 'lucide-react';
+import { ClassNameValue } from 'tailwind-merge';
 import IconComponent from './icon-component';
 
-export const FeaturedStar = ({ project }: { project: Project }) => {
+export const FeaturedStar = ({ project, className }: { project: Project; className?: ClassNameValue }) => {
     const toggleFeatured = (project: Project) => {
         router.put(`/projects/${project.id}`, { ...project, featured: !project.featured });
     };
 
     return (
         <Star
-            className={cn('m-auto h-4 w-4 cursor-pointer text-yellow-500', project.featured ? 'fill-yellow-500' : '')}
+            className={cn('m-auto h-4 w-4 cursor-pointer text-yellow-500', project.featured ? 'fill-yellow-500' : '', className)}
             onClick={(e) => {
                 e.stopPropagation();
                 toggleFeatured(project);
