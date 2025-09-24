3b0369a79e0db58a3e3a2a7cbbc328c934a1bddb
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-09-24 07:32:07 -0400

    created migrations to add icon_name to technologies and change all icon_name columns to new enum LucideReact

---
+++ database/migrations/2025_09_24_112534_change_icon_name_culumns_to_enum_type.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\LucideIcon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (DB::getDriverName() !== 'sqlite') {
+            // Use enum for MySQL/PostgreSQL
+
+            Schema::table('projects', function (Blueprint $table) {
+                $table->enum('icon_name', array_column(LucideIcon::cases(), 'value'))->nullable()->change();
+            });
+
+            Schema::table('skills', function (Blueprint $table) {
+                $table->enum('icon_name', array_column(LucideIcon::cases(), 'value'))->nullable()->change();
+            });
+
+            Schema::table('technologgies', function (Blueprint $table) {
+                $table->enum('icon_name', array_column(LucideIcon::cases(), 'value'))->nullable()->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Change icon_name column back to string type
+
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('icon_name')->nullable()->change();
+        });
+
+        Schema::table('skills', function (Blueprint $table) {
+            $table->string('icon_name')->nullable()->change();
+        });
+
+        Schema::table('technologies', function (Blueprint $table) {
+            $table->string('icon_name')->nullable()->change();
+        });
+    }
+};
