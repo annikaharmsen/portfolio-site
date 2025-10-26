91dc2b2e0c46209471d02afa75c700bc4aa06a98
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-10-26 17:29:23 -0400

    refactor: implemented persistent default layout for all admin/ page components

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,15 +1,29 @@
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ComponentType, ReactElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
+import AppLayout from './layouts/app-layout';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+type InertiaPageModule = {
+    default: ComponentType & {
+        layout?: (page: ReactElement) => ReactElement;
+    };
+};
+
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    resolve: (name) => {
+        const pages = import.meta.glob<InertiaPageModule>('./pages/**/*.tsx', {
+            eager: true,
+        });
+        const page = pages[`./pages/${name}.tsx`];
+        page.default.layout = name.startsWith('admin/') ? (page) => <AppLayout children={page} /> : undefined;
+        return page;
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
 
