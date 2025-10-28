a67c1664cbd2d959e5fe38c08ba8e2ad9472d0fa
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-10-28 18:41:24 -0400

    fix: modified hero sections update method to reroute to project edit

--- app/Http/Controllers/ProjectHeroSectionsController.php
+++ app/Http/Controllers/ProjectHeroSectionsController.php
@@ -32,6 +32,6 @@ class ProjectHeroSectionsController extends Controller
         // Delete sections not in submission
         $project->hero_sections()->whereNotIn('id', $submittedIDs)->delete();
 
-        return redirect(route('projects.show', [$project]));
+        return redirect(route('projects.edit', [$project]));
     }
 }
