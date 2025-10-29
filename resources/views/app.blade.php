0f1453dd54bfacb90348351d777d7d9841d86570
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-10-29 14:19:49 -0400

    fix: remove individual page loading from Vite  directive

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -43,7 +43,7 @@
         <!-- react routing -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @vite(['resources/js/app.tsx'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
