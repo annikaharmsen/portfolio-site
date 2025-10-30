713baabe8e6fc5f7f05a4e96778544621455f19c
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-10-30 11:09:41 -0400

    fix: filter projects to display only featured ones in ProjectsSection component

--- resources/js/components/portfolio/projects-section.tsx
+++ resources/js/components/portfolio/projects-section.tsx
@@ -2,12 +2,17 @@ import ProjectCard from '@/components/portfolio/project-card';
 import { Projects } from '@/types/models';
 
 export default function ProjectsSection({ projects }: { projects: Projects }) {
+    const featuredProjects = projects.filter((project) => project.featured);
+    if (featuredProjects.length < 1) return;
+
     return (
         <section id="projects" className="bg-muted py-16">
             <div className="mx-auto max-w-6xl px-4 sm:px-6">
                 <h2 className="mb-12 text-center text-4xl uppercase">Projects</h2>
                 <div className="grid grid-cols-1 gap-6 sm:gap-8 lg:grid-cols-2">
-                    {projects && projects.map((project) => <ProjectCard key={project.id} project={project} />)}
+                    {featuredProjects.map((project) => (
+                        <ProjectCard key={project.id} project={project} />
+                    ))}
                 </div>
             </div>
         </section>
