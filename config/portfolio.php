976bc153f64233d722fab262f9361823d367ca65
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-10-31 12:48:22 -0400

    feat: add resume configuration and update resume route to use config

---
+++ config/portfolio.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Resume Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Configure the resume file for download on the portfolio page.
+    |
+    */
+
+    'resume' => [
+        'filename' => env('RESUME_FILENAME'),
+    ],
+];
