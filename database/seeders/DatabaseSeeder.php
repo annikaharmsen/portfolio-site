d40a59dcf72196fdc04da8dfd74c6ecadfd9101a
Annika Harmsen <annikat.harmsen1000@gmail.com>
2026-06-10 09:36:49 -0400

    feat: add experience seeder with previously hard-coded job data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Run demo seeder for demo database
-        $this->call(DemoSeeder::class);
+        $this->call([
+            ExperienceSeeder::class,
+        ]);
     }
 }
