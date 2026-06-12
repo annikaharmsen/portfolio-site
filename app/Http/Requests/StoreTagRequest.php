4c1cce1913df997bfb0c25f486249dce03d9e8b4
Annika Harmsen <annikat.harmsen1000@gmail.com>
2026-06-12 07:02:17 -0400

    feat: add skill_group_id validation to tag store/update requests

--- app/Http/Requests/StoreTagRequest.php
+++ app/Http/Requests/StoreTagRequest.php
@@ -14,6 +14,7 @@ class StoreTagRequest extends CmsRequest
             'name' => 'required|string|min:1|max:255',
             'projects' => 'array|distinct|exists:projects,id',
             'category' => 'nullable|string|max:255',
+            'skill_group_id' => 'nullable|integer|exists:skill_groups,id',
         ];
     }
 
